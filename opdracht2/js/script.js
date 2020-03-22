5977ea98ed1694643bdf1263a9b1a0e63552a130
r-nold <r-nold@users.noreply.github.com>
2020-03-22 01:09:36 +0100

    Opdracht 2 voltooid

--- opdracht2/js/script.js
+++ opdracht2/js/script.js
@@ -1,19 +1,61 @@
-window.addEventListener("scroll", function() {
-  console.log("Scrolled");
+let alleportfolio = document.querySelectorAll(".portfolio-item");
+
+window.addEventListener("scroll", () => {
+  const scrolledlocal = window.scrollY;
+  if (scrolledlocal < 700) {
+    /*     portfolio.classList.remove("appear");
+    portfolio.classList.add("okbye"); */
+    alleportfolio.forEach(pitem => {
+      pitem.classList.add("okbye");
+      pitem.classList.remove("appear");
+    });
+  } else {
+    alleportfolio.forEach(pitem => {
+      pitem.classList.add("appear");
+      pitem.classList.remove("okbye");
+    });
+    /*     portfolio.classList.add("appear");
+    portfolio.classList.remove("okbye"); */
+  }
+});
+/* portfolio.style.display = "flex"; */
+let portfolio = document.querySelector(".portfolio");
+
+console.log(alleportfolio);
+
+// Get the input field
+var input = document.querySelector(".body-fixed");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 9) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.querySelector(".more").click();
+  }
 });
 
-let scrollBtn = document.querySelector(".scrollbtn");
-let body = document.querySelector(".scrollArea");
-let nav = document.querySelectorAll(".nav");
+var grammar =
+  "#JSGF V1.0; grammar options; public <option> = up | down | poop;";
+var recognition = new webkitSpeechRecognition();
+var speechRecognitionList = new webkitSpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = "en-US";
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
 
-scrollBtn.addEventListener("click", function() {
-  body.classList.toggle("smooth-up");
-});
+var diagnostic = document.querySelector(".output");
 
-navup.addEventListener("click", function() {
-  navup.classList.toggle("smooth-up");
-});
+recognition.start();
+console.log("Ready to receive a voice command.");
 
-navdown.addEventListener("click", function() {
-  navdown.classList.toggle("smooth-down");
-});
+recognition.onresult = function(event) {
+  let option = event.results[0][0].transcript;
+  console.log(option);
+  let test = document.querySelector("#" + String(option)).click();
+  console.log(test);
+};
