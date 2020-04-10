eef92db7bbf351821ed728cfcd712e354e301098
r-nold <r-nold@users.noreply.github.com>
2020-04-10 20:53:11 +0200

    Old API was offline, switched to new API

--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -4,38 +4,37 @@ let htmlNewCases = document.querySelector(".todayCasesHtml");
 let htmlDeaths = document.querySelector(".deathsHtml");
 let htmlNewDeaths = document.querySelector(".todayDeathsHtml");
 
-// Bron van de API
-const urlapi = "https://coronavirus-19-api.herokuapp.com/countries/netherlands";
+function getTheData() {
+  // Bron van de API en het ophalen ervan
+  var apiData = fetch(
+    "https://covid-19-data.p.rapidapi.com/country?format=undefined&name=netherlands",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+        "x-rapidapi-key": "795ebcbd3emshfbe78a7408abecdp12d4b3jsn286d61d78529",
+      },
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      var storedData = data[0];
+      console.log(storedData);
+      let cases = document.createElement("H2");
+      let numberofcases = document.createTextNode(storedData.confirmed);
+      cases.appendChild(numberofcases);
+      htmlCases.appendChild(cases);
 
-// Ophalen van de API gegevens in JSON formaat en dit in de variabele nld zetten
-fetch(urlapi)
-  .then(response => {
-    return response.json();
-  })
-  .then(nld => {
-    // Het aanmaken van de HTML elementen om later de data aan toe te voegen
-    let cases = document.createElement("H2");
-    let numberofcases = document.createTextNode(nld.cases);
+      let deaths = document.createElement("H2");
+      let numberofdeaths = document.createTextNode(storedData.deaths);
+      deaths.appendChild(numberofdeaths);
+      htmlDeaths.appendChild(deaths);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
-    let newcases = document.createElement("H2");
-    let numberofnewcases = document.createTextNode(nld.todayCases);
-
-    let deaths = document.createElement("H2");
-    let numberofdeaths = document.createTextNode(nld.deaths);
-
-    let newdeaths = document.createElement("H2");
-    let numberofnewdeaths = document.createTextNode(nld.todayDeaths);
-
-    //Het toevoegen van de opgehaalde gegevens aan nieuw aangemaakte H2 elementen met daarin nieuwe text nodes
-    cases.appendChild(numberofcases);
-    htmlCases.appendChild(cases);
-
-    newcases.appendChild(numberofnewcases);
-    htmlNewCases.appendChild(newcases);
-
-    deaths.appendChild(numberofdeaths);
-    htmlDeaths.appendChild(deaths);
-
-    newdeaths.appendChild(numberofnewdeaths);
-    htmlNewDeaths.appendChild(newdeaths);
-  });
+getTheData();
