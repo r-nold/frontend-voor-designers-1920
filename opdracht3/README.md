22eccb9d69b24cc66beb0f96d374dd7a27c3e8c9
r-nold <r-nold@users.noreply.github.com>
2020-04-17 03:36:14 +0200

    Eindopdracht v0.9 - Klaar voor beoordeling

--- opdracht3/README.md
+++ opdracht3/README.md
@@ -2,7 +2,7 @@
 
 ## Door Arnie Schrage
 
-Stel je eens voor, er breekt een enorme pandemie uit waardoor je verplicht thuis moet zitten. Dan zou het zo maar eens kunnen dat je door die absurde hoeveelheid tijd die je aan games besteedt (Jep, jij ook) ineens klaar bent. Je platinum trophies zijn binnen, die ene sidequest is gehaald en je speedrun heeft al eens het wereldrecord verslagen.
+Stel je eens voor, er breekt een enorme pandemie uit waardoor je verplicht thuis moet zitten. Dan zou het zo maar eens kunnen dat je door die absurde hoeveelheid tijd die je aan games besteedt (Jep, jij ook) ineens klaar bent. Je platinum trophies zijn binnen, die ene sidequest is gehaald en je speedrun heeft al eens het wereldrecord verbroken.
 
 "OH NEE, WAT NU?!" hoor ik je al zeggen.
 
@@ -10,4 +10,14 @@ NextPlay helpt jou in deze extreem lastige situatie, je staat er niet alleen voo
 
 Wanneer je even geen inspiratie hebt en niet weet welke game je moet spelen, kan NextPlay een suggestie geven. Vul de filters in, en voor je het weet ben je alweer aan het grinden voor die achievements! #Covidgaming
 
--Arnie
+## Techniek
+
+De API is van RAWG, een grote videogame database. Deze api wordt aangesproken nadat de gebruiker zijn filters heeft toegepast. Er worden op basis van de ingevulde opties variabelen aangemaakt, die vervolgens worden omgezet naar een URI. De response van de API wordt in een array opgeslagen, die vervolgens gebruikt wordt om de pagina dynamisch in te vullen met de content die is opgevraagd.
+
+## GitHub
+
+Het gebruik van GitHub heb ik gaandeweg beter leren inzetten. Tot nu toe heb ik het vooral gebruikt om gewoon mijn voortgang op te slaan. Wanneer ik beter wordt met programmeren zal ik het meer gaan gebruiken om bijv. nieuwe features te schrijven en deze te pushen.
+
+## Volgende stappen
+
+Deze tool kan nog veel verbeterd worden. Als ik het nog verder zou uitbreiden, zou ik willen kijken of ik data van meerdere api's zou kunnen samenvoegen, zodat er bijv. automatisch een linkje komt om de game te kopen (Uiteraard waar die het goedkoopst is), ik zou het geheel visueel willen verbeteren, en de rating functie werkend willen krijgen. Ook wil ik nog wat verder verdiepen in het opzetten van queries zodat de filters geavanceerder zijn, en misschien wel realtime in beeld komen. Dit niveau heb ik helaas nog niet bereikt. Ik zou liever de code willen leren optimaliseren, want waarschijnlijk kan het efficienter, maar goed, voor een prototyping vak gaat het nog wel. En gezien ik bij de eerste les nog maar net een Eventlistener kon toepassen, zie ik dat mijn JS niveau een aardige sprong heeft gemaakt!
--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -1,31 +1,155 @@
-// Aanmaken van variabelen om de HTML elementen op te halen
-let htmlCases = document.querySelector(".casesHtml");
-let htmlNewCases = document.querySelector(".todayCasesHtml");
-let htmlDeaths = document.querySelector(".deathsHtml");
-let htmlNewDeaths = document.querySelector(".todayDeathsHtml");
+//Functie om ervoor te zorgen dat er maar 1 enkele optie geselecteerd kan worden
+function selectedCheck() {
+  if (ps4selected == true) {
+    ps4selected = !ps4selected;
+    ps4.classList.remove("selected");
+  }
+  if (xboxselected == true) {
+    xboxselected = !xboxselected;
+    xbox.classList.remove("selected");
+  }
+  if (pcselected == true) {
+    pcselected = !pcselected;
+    pc.classList.remove("selected");
+  }
+  if (switchselected == true) {
+    switchselected = !switchselected;
+    nswitch.classList.remove("selected");
+  }
+}
 
-function getTheData() {
+//Hulptool om uit te lezen of de variabelen goed gemanipuleerd worden
+function logger() {
+  console.clear();
+  console.log("ps4:" + ps4selected);
+  console.log("xbox:" + xboxselected);
+  console.log("pc:" + pcselected);
+  console.log("switch:" + switchselected);
+}
+
+//API functie
+function getResults() {
   // Bron van de API en het ophalen ervan
-  var apiData = fetch(
-    "https://rawg-video-games-database.p.rapidapi.com/games",
-    {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-        "x-rapidapi-key": "795ebcbd3emshfbe78a7408abecdp12d4b3jsn286d61d78529",
-      },
-    }
-  )
+  var url = new URL("https://rawg-video-games-database.p.rapidapi.com/games");
+
+  // De parameters die in de query gezet worden, dit gebeurt dynamisch op basis van input van de gebruiker
+  var params = {
+    platforms: cplatform,
+    rating: crating,
+    genres: cgenre,
+    ordering: "-rating",
+  };
+  //Het toevoegen van de parameters in de url
+  url.search = new URLSearchParams(params).toString();
+  // API keys voor authorisatie via RapidApi
+  var apiData = fetch(url, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+      "x-rapidapi-key": "795ebcbd3emshfbe78a7408abecdp12d4b3jsn286d61d78529",
+    },
+  }) //Omzetten van de response naar json format
     .then((response) => {
       return response.json();
     })
+    //opslaan van de json data in variabele in de vorm van array
     .then((data) => {
       var storedData = data;
-      console.log(storedData.results[0].metacritic);
+      gameX = [
+        storedData.results[0],
+        storedData.results[1],
+        storedData.results[2],
+        storedData.results[3],
+        storedData.results[4],
+        storedData.results[5],
+      ];
+
+      //Om meerdere opties te laten zien, worden de 6 topresultaten willekeurig gemaakt
+      //zodat er voldoende afwisseling is
+      const randomGame = gameX[Math.floor(Math.random() * gameX.length)];
+      //De game wordt in de html gezet, en scrollt hierna automatisch naar de game
+      var gamePlace = document.querySelector(".gamePlace");
+      var gameImg = randomGame.background_image;
+      gamePlace.innerHTML = "<h1>" + randomGame.name + "</h1>";
+      gamePlace.style.backgroundImage = "url(" + gameImg + ")";
+      gamePlace.style.backgroundSize = "contain";
+      gamePlace.style.backgroundRepeat = "no-repeat";
+      gamePlace.style.height = "600px";
+      window.location.href = "#game";
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
-getTheData();
+//De filters (Slider, buttons en dropdown)
+var slider = document.getElementById("rating");
+var output = document.getElementById("demo");
+let go = document.querySelector(".go");
+let genre_element = document.querySelector("#genre");
+
+var cplatform;
+var cgenre = "action";
+var crating;
+
+var ps4selected = false;
+var xboxselected = false;
+var pcselected = false;
+var switchselected = false;
+
+let ps4 = document.querySelector(".ps4");
+let xbox = document.querySelector(".xbox");
+let pc = document.querySelector(".pc");
+let nswitch = document.querySelector(".switch");
+
+output.innerHTML = slider.value;
+slider.oninput = function () {
+  output.innerHTML = this.value;
+  crating = slider.value;
+};
+
+// Event listeners om te kijken welke opties geselecteerd worden
+ps4.addEventListener("click", function (hoi) {
+  selectedCheck();
+  ps4selected = true;
+  if (ps4selected == true) {
+    cplatform = 18;
+    ps4.classList.add("selected");
+  }
+});
+xbox.addEventListener("click", function (hoi) {
+  selectedCheck();
+  xboxselected = true;
+  if (xboxselected == true) {
+    cplatform = 1;
+    xbox.classList.add("selected");
+  }
+});
+pc.addEventListener("click", function (hoi) {
+  selectedCheck();
+  pcselected = true;
+  if (pcselected == true) {
+    cplatform = 4;
+    pc.classList.add("selected");
+  }
+});
+nswitch.addEventListener("click", function (hoi) {
+  selectedCheck();
+  switchselected = true;
+  if (switchselected == true) {
+    cplatform = 7;
+    nswitch.classList.add("selected");
+  }
+});
+
+genre_element.oninput = function () {
+  cgenre = this.value;
+};
+
+//De grote playbutton die alles in gang zet
+go.addEventListener("click", function (iets) {
+  /*   console.log(cplatform);
+  console.log(cgenre);
+  console.log(crating); */
+  getResults();
+});
